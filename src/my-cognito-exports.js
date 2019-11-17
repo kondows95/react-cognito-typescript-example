dde7543603cac1f4f38b087ea4fa641c45510075
Tsubasa Kondo <kondows95@gmail.com>
2019-11-18 04:46:53 +0630

    gitignore my-cognito-exports.js

--- src/my-cognito-exports.js
+++
@@ -1,9 +0,0 @@
-
-export default  {
-    "aws_project_region": "ap-northeast-1",
-    "aws_cognito_identity_pool_id": "ap-northeast-1:79baf745-24ff-4e3f-8500-abeb06be6a81",
-    "aws_cognito_region": "ap-northeast-1",
-    "aws_user_pools_id": "ap-northeast-1_PKHkwHRk6",
-    "aws_user_pools_web_client_id": "1amqclauqrokfds34ipob9956s",
-    "oauth": {}
-};
